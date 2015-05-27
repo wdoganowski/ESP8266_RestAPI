b21641ab71fc11d03ce2715768a88de9fed7e24e
wdoganowski <wojciech@doganowscy.com>
2015-05-27 07:10:52 +0200

    HTML setup page
    When no setup in EEPROM, start as AP, otherwise use EEPROM setup to
    connect to known SSID

--- restapi_test/restapi_test.ino
+++ restapi_test/restapi_test.ino
@@ -34,14 +34,34 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#define DEBUG_TO_SERIAL_PORT // When defined, the debug output is sent to serial at 74880 baud, otherwise the blue LED blinks on activity
+#include "root_html.h"
 
-#define SSIDNAME "ESP8266xxx"
+#define __DEBUG_TO_SERIAL_PORT // When defined, the debug output is sent to serial at 74880 baud, otherwise the blue LED blinks on activity
+
+#define SSIDNAME "ESP"
 #define PASSWORD ""
-#define SSID_PASS_SIZE 32
+#define SSID_PASS_SIZE 16
+#define LOW_HIGH_SIZE 16
+#define DATA_HEADER 0x5A
 
-char ssid[SSID_PASS_SIZE];
-char password[SSID_PASS_SIZE];
+typedef struct SetupStruct {
+  char header;
+  byte start_counter;
+  char ssid[SSID_PASS_SIZE];
+  char password[SSID_PASS_SIZE];
+  char dns_name[SSID_PASS_SIZE];
+  byte ap_mode;
+  byte gpio0_mode;
+  byte gpio0_value;
+  char gpio0_low2high[LOW_HIGH_SIZE];
+  char gpio0_high2low[LOW_HIGH_SIZE];
+  byte gpio2_mode;
+  byte gpio2_value;
+  char gpio2_low2high[LOW_HIGH_SIZE];
+  char gpio2_high2low[LOW_HIGH_SIZE];
+} setup_t;
+
+setup_t setup_data;
 IPAddress myIP;
 MDNSResponder mdns;
 
@@ -60,33 +80,34 @@ ESP8266WebServer server ( 80 );
 #define DEBUGLN( message ) {}
 #endif
 
+/*
+ * Web server
+ */
+ 
 void handleRoot() {
-	DEBUGLED( 1, 0 );
-	char temp[400];
+	DEBUGLED( LED_ON, 0 );
+	char temp[3000];
 	int sec = millis() / 1000;
 	int min = sec / 60;
 	int hr = min / 60;
 
-	snprintf ( temp, 400,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-  </body>\
-</html>",
-
-		hr, min % 60, sec % 60
-	);
+  handleSetup();
+  
+	snprintf ( temp, 3000, root_html, 
+	  hr, min % 60, sec % 60, 
+	  myIP[0], myIP[1], myIP[2], myIP[3], 
+	  setup_data.ssid, setup_data.password, setup_data.dns_name, 
+	  setup_data.ap_mode?"checked":"", setup_data.ap_mode?"":"checked",
+	  setup_data.gpio0_mode?"":"checked", setup_data.gpio0_mode?"checked":"",
+	  setup_data.gpio0_value?"":"checked", setup_data.gpio0_value?"checked":"",
+	  setup_data.gpio0_low2high, setup_data.gpio0_high2low,
+    setup_data.gpio2_mode?"":"checked", setup_data.gpio2_mode?"checked":"",
+    setup_data.gpio2_value?"":"checked", setup_data.gpio2_value?"checked":"",
+    setup_data.gpio2_low2high, setup_data.gpio2_high2low
+	  );
 	server.send ( 200, "text/html", temp );
-	DEBUGLED( 0, 0 );
+
+	DEBUGLED( LED_OFF, 0 );
 }
 
 void handleNotFound() {
@@ -100,7 +121,7 @@ void handleNotFound() {
 	message += server.args();
 	message += "\n";
 
-	for ( uint8_t i = 0; i < server.args(); i++ ) {
+	for ( byte i = 0; i < server.args(); i++ ) {
 		message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
 	}
 
@@ -109,34 +130,221 @@ void handleNotFound() {
 	DEBUGLED( LED_OFF, 0 );
 }
 
-int ReadSetup( char* ssid, char* password ) {
-  int addr = 0;
-  byte value;
+/*
+ * Handling setup parameters from URI
+ */
 
-  value = EEPROM.read(addr); addr++;
-  if( value = 0xA5 ) {
+void handleSetup() {
+  DEBUGLED( LED_ON, 0 );
+  int reboot = 0;
+  int reset_data = 0;
+
+  if( server.args() ) {
+
+    DEBUG__( "Handle Setup with # of args " );
+    DEBUGLN( server.args() );
+
+    for ( byte i = 0; i < server.args(); i++ ) {
+      DEBUGLN( server.argName(i) + ": " + server.arg(i) );
+
+      yield();
+
+      // WiFi  
+      if ( server.argName(i) == "ssid" ) {
+        strncpy( setup_data.ssid, server.arg(i).c_str(), SSID_PASS_SIZE );
+        setup_data.ssid[SSID_PASS_SIZE-1] = 0;
+      } else if ( server.argName(i) == "pass" ) {
+        strncpy( setup_data.password,  server.arg(i).c_str(), SSID_PASS_SIZE );
+        setup_data.password[SSID_PASS_SIZE-1] = 0;
+      } else if ( server.argName(i) == "dns" ) {
+        strncpy( setup_data.dns_name,  server.arg(i).c_str(), SSID_PASS_SIZE );
+        setup_data.dns_name[SSID_PASS_SIZE-1] = 0;
+      } else if ( server.argName(i) == "ap_mode" ) {
+        if ( server.arg(i) == "yes" ) {
+          setup_data.ap_mode = 1;
+        } else {
+          setup_data.ap_mode = 0;
+        }
+      } 
+
+      // GPIO0
+      if ( server.argName(i) == "gpio0_mode" ) {
+        setup_data.gpio0_mode = server.arg(i).toInt();
+      } else if ( server.argName(i) == "gpio0_value" ) {
+        setup_data.gpio0_value = server.arg(i).toInt();
+      } else if ( server.argName(i) == "gpio0_low2high" ) {
+        strncpy( setup_data.gpio0_low2high, server.arg(i).c_str(), LOW_HIGH_SIZE );
+        setup_data.gpio0_low2high[LOW_HIGH_SIZE-1] = 0;
+      } else if ( server.argName(i) == "gpio0_high2low" ) {
+        strncpy( setup_data.gpio0_high2low, server.arg(i).c_str(), LOW_HIGH_SIZE );
+        setup_data.gpio0_high2low[LOW_HIGH_SIZE-1] = 0;
+      } 
+
+      // GPIO2
+      if ( server.argName(i) == "gpio2_mode" ) {
+        setup_data.gpio2_mode = server.arg(i).toInt();
+      } else if ( server.argName(i) == "gpio2_value" ) {
+        setup_data.gpio2_value = server.arg(i).toInt();
+      } else if ( server.argName(i) == "gpio2_low2high" ) {
+        strncpy( setup_data.gpio2_low2high, server.arg(i).c_str(), LOW_HIGH_SIZE );
+        setup_data.gpio2_low2high[LOW_HIGH_SIZE-1] = 0;
+      } else if ( server.argName(i) == "gpio2_high2low" ) {
+        strncpy( setup_data.gpio2_high2low, server.arg(i).c_str(), LOW_HIGH_SIZE );
+        setup_data.gpio2_high2low[LOW_HIGH_SIZE-1] = 0;
+      }   
+
+      // Reboot & Reset
+      if ( server.argName(i) == "reset" ) {
+        if ( server.arg(i) == "yes" ) {
+          reset_data = 1;
+        }
+      } else if ( server.argName(i) == "reboot" ) {
+        if ( server.arg(i) == "yes" ) {
+          reboot = 1;
+        }
+      } 
+    } 
+
+    if( reset_data ) ResetSetup();
+    else WriteSetup();
+  
+    if (reboot) {
+      server.send ( 200, "text/html", 
+       "<html><head><meta http-equiv='refresh' content='15'/><title>ESP8266 PIO Bridge</title>\
+        <style>body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>\
+        </head>\
+        <body><h1>REBOOTING...</h1></body>\
+        </html>" 
+      );
+      // reset here
+      ESP.restart();
+    }
+  
+  }
+  
+  DEBUGLED( LED_OFF, 0 );
+}
+
+/*
+ * EEPROM Reset/Reading/Writing
+ */
+
+void ResetSetup() {
+  DEBUGLED( LED_ON, 0 );
+  
+  char *ptr = (char *) &setup_data;
+
+  DEBUGLN( "Reset EEPROM " );
+
+  yield();
+
+  // Default values for AP
+  setup_data.header = DATA_HEADER;
+  setup_data.start_counter = 0; 
+  byte mac[6];
+  WiFi.macAddress(mac); // Read MAC
+  sprintf( setup_data.ssid, "%s_%X%X%X", SSIDNAME, mac[3], mac[4], mac[5] );
+  strcpy( setup_data.password, PASSWORD );
+  strcpy( setup_data.dns_name, setup_data.ssid );
+  setup_data.ap_mode = 1;
+  
+  setup_data.gpio0_mode = 0;
+  setup_data.gpio0_value = 0;
+  strcpy( setup_data.gpio0_low2high, "" );
+  strcpy( setup_data.gpio0_high2low, "" );
+  
+  setup_data.gpio2_mode = 0;
+  setup_data.gpio2_value = 0;
+  strcpy( setup_data.gpio2_low2high, "" );
+  strcpy( setup_data.gpio2_high2low, "" );
+  
+  WriteSetup();
+  
+  DEBUGLED( LED_OFF, 0 );
+}
+ 
+void ReadSetup() {
+  DEBUGLED( LED_ON, 0 );
+  
+  char *ptr = (char *) &setup_data;
+
+  DEBUG__( "Reading EEPROM (" );
+  DEBUG__( sizeof(setup_t) );
+  DEBUGLN( " bytes) " );
+/*  for ( int addr=0; addr<sizeof(setup_t); addr++ ) {
+    ptr[addr] = EEPROM.read(addr);
+#ifdef DEBUG_TO_SERIAL_PORT
+    Serial.print ( ptr[addr], HEX );
+    Serial.write ( ptr[addr] );
+    Serial.println();
+#endif
+  }
+*/  
+  memcpy( &setup_data, EEPROM.getDataPtr(), sizeof(setup_t) );
+  DEBUGLN( "" );
+  
+  if( setup_data.header == DATA_HEADER ) {
     // We have the setup
-    // Read SSID
-    for( int i=0; i<SSID_PASS_SIZE; i++ ) {
-      ssid[i] = EEPROM.read(addr);
-      addr++;
-    }
-    // Read PASS
-    for( int i=0; i<SSID_PASS_SIZE; i++ ) {
-      password[i] = EEPROM.read(addr);
-      addr++;
-    }
-    return 1;
   } else {
     // Default values for AP  
-    strcpy( ssid, SSIDNAME );
-    strcpy( password, PASSWORD );
-    return 0;
+    ResetSetup();
+  }
+  
+  DEBUGLED( LED_OFF, 0 );
+}
+
+void EEPROM_Update( int address, uint8_t value ) {
+  if ( EEPROM.read(address) != value ) {
+    EEPROM.write(address, value);
   }
 }
 
+void WriteSetup() {
+  DEBUGLED( LED_ON, 0 );
+  
+  char *ptr = (char *) &setup_data;
+
+  DEBUG__( "Writing EEPROM (" );
+  DEBUG__( sizeof(setup_t) );
+  DEBUGLN( " bytes) " );
+
+  yield();
+  
+  for( int addr=0; addr<sizeof(setup_t); addr++ ) {
+    EEPROM_Update(addr, ptr[addr]);
+#ifdef DEBUG_TO_SERIAL_PORT
+    Serial.print ( ptr[addr], HEX );
+    Serial.write ( ptr[addr] );
+    Serial.println();
+#endif
+  }
+  DEBUGLN( "" );
+  DEBUG__( "EEPROM Commit " );  
+/*  while( !EEPROM.commit() ) {
+    DEBUG__( "." );
+    DEBUGLED( LED_OFF, 100 );    
+    DEBUGLED( LED_ON, 100 ); 
+    DEBUGLED( LED_OFF, 0 );   
+    delay( 500 );
+    DEBUGLED( LED_ON, 0 );
+  } */
+  DEBUGLN( " Done" );  
+  delay(100);
+  
+  DEBUGLED( LED_OFF, 0 );
+}
+
+void StartCounter( byte counter ) {
+  setup_data.start_counter = counter;
+  EEPROM.write(1, counter);
+  EEPROM.commit();
+}
+
+/*
+ * Setup
+ */
+ 
 void setup ( void ) {
-  delay(1000);
   
   // Init PIO
 #ifndef DEBUG_TO_SERIAL_PORT
@@ -146,10 +354,11 @@ void setup ( void ) {
   DEBUGLED( LED_OFF, 0 );
 
 #ifdef DEBUG_TO_SERIAL_PORT
+  delay(5000);
   Serial.begin ( 74880 );
   // Init Serial
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    delay( 1 ); // wait for serial port to connect. Needed for Leonardo only
   }
 #endif
 
@@ -157,21 +366,55 @@ void setup ( void ) {
   DEBUGLN( "ESP8266 REST API 1.0" );
   
   // Init EEPROM
-  EEPROM.begin(512);
-  if( ReadSetup( ssid, password ) ) {
-    
-    DEBUGLN( "Setup Exisists for AP:" ); 
-    DEBUGLN( ssid );
+  EEPROM.begin( sizeof( setup_t ) );
+  ReadSetup();
+  if( setup_data.start_counter > 3 ) {
+    DEBUGLN( "*** RESET SETUP ***" );
+    for( int i=100; i<1000; i+=100 ) {
+      DEBUGLED( LED_ON, i );
+      DEBUGLED( LED_OFF, i );
+    }
+    ResetSetup();
+  } else {
+    StartCounter( setup_data.start_counter + 1 );
+  }
+  
+  if( setup_data.ap_mode )
+  {    
+   
+    DEBUGLN( "Staring in AP mode:" );
+    DEBUGLN( setup_data.ssid );
+    WiFi.softAP( setup_data.ssid ); 
+ 
+    // Wait for setup
+    /*while ( WiFi.status() != WL_CONNECTED ) {
+      DEBUGLED( LED_ON, 250 );
+      DEBUGLED( LED_OFF, 0 );
+      delay( 500 );
+      DEBUG__( "." );
+    } */ 
+
+    myIP = WiFi.softAPIP();
+
+    DEBUGLN( "" );
+    DEBUG__( "Created AP " );
+    DEBUGLN( setup_data.ssid );
+    DEBUG__( "IP address: " );
+    DEBUGLN( myIP );   
+
+  } else {
+
+    DEBUG__( "Connection to SSID: " ); 
+    DEBUGLN( setup_data.ssid );
 
     // Init WiFi
-    DEBUGLN( "Connecting to WiFi" );
-    WiFi.begin ( ssid, password );
+    WiFi.begin ( setup_data.ssid, setup_data.password );
 
     // Wait for connection
     while ( WiFi.status() != WL_CONNECTED ) {
       DEBUGLED( LED_ON, 250 );
       DEBUGLED( LED_OFF, 0 );
-      delay( 500);
+      delay( 500 );
       DEBUG__( "." );
     }
 
@@ -179,35 +422,20 @@ void setup ( void ) {
     
     DEBUGLN( "" );
     DEBUG__( "Connected to " );
-    DEBUGLN( ssid );
+    DEBUGLN( setup_data.ssid );
     DEBUG__( "IP address: " );
     DEBUGLN( myIP );
   
-  } else {
-
-    DEBUGLN( "Staring in AP mode:" );
-    DEBUGLN( ssid );
-    WiFi.softAP( ssid, password ); 
- 
-    // Wait for setup
-    while ( WiFi.status() != WL_CONNECTED ) {
-      DEBUGLED( LED_ON, 250 );
-      DEBUGLED( LED_OFF, 0 );
-      delay( 500);
-      DEBUG__( "." );
-    }  
-
-    myIP = WiFi.softAPIP();
-
-    DEBUGLN( "" );
-    DEBUG__( "Created AP " );
-    DEBUGLN( ssid );
-    DEBUG__( "IP address: " );
-    DEBUGLN( myIP );   
   }
 
+#ifdef DEBUG_TO_SERIAL_PORT
+  DEBUGLN( "==========" );
+  WiFi.printDiag(Serial);
+  DEBUGLN( "==========" );
+#endif
+
   // Init mDNS
-	if ( mdns.begin ( ssid, myIP ) ) {
+	if ( setup_data.dns_name[0] && mdns.begin( setup_data.dns_name, myIP ) ) {
     DEBUGLED( LED_ON, 1000 );
     DEBUGLED( LED_OFF, 1000 );		
     DEBUGLN( "MDNS responder started" );
@@ -215,9 +443,6 @@ void setup ( void ) {
 
   // Init HTTP Server
 	server.on ( "/", handleRoot );
-	server.on ( "/inline", []() {
-		server.send ( 200, "text/plain", "this works as well" );
-	} );
 	server.onNotFound ( handleNotFound );
 	server.begin();
   DEBUGLED( LED_ON, 500 );
@@ -225,10 +450,40 @@ void setup ( void ) {
   DEBUGLED( LED_ON, 1000 );
   DEBUGLED( LED_OFF, 1000 );   
 	DEBUGLN( "HTTP server started" );
+
+  pinMode( 0, setup_data.gpio0_mode?INPUT:OUTPUT );
+  pinMode( 2, setup_data.gpio2_mode?INPUT:OUTPUT );
+
+  // Everything OK
+  StartCounter( 0 );
 }
 
 void loop ( void ) {
-	mdns.update();
+	if( setup_data.dns_name[0] ) mdns.update();
 	server.handleClient();
+
+  if( setup_data.gpio0_mode == 0 ) {
+    // Input
+    setup_data.gpio0_value = digitalRead( 0 );
+    DEBUG__( "GPIO0 read " );
+    DEBUGLN( setup_data.gpio0_value );
+  } else {
+    // Output
+    digitalWrite( 0, setup_data.gpio0_value );
+    DEBUG__( "GPIO0 write " );
+    DEBUGLN( setup_data.gpio0_value );
+  }
+
+  if( setup_data.gpio2_mode == 0 ) {
+    // Input
+    setup_data.gpio2_value = digitalRead( 2 );
+    DEBUG__( "GPIO2 read " );
+    DEBUGLN( setup_data.gpio2_value );
+  } else {
+    // Output
+    digitalWrite( 2, setup_data.gpio2_value );
+    DEBUG__( "GPIO2 write " );
+    DEBUGLN( setup_data.gpio2_value );
+  }
 }
 
---
+++ restapi_test/root_html.h
@@ -0,0 +1,73 @@
+static const char root_html[] = "\
+<html>\
+  <head>\
+    <title>ESP8266 PIO Bridge</title>\
+    <style>body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style>\
+  </head>\
+  <body>\
+    <div style='background-color:lightgrey; padding:20px;'>\
+    <h1>ESP8266 PIO Bridge</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+    </div>\
+\
+    <div padding:20px><br></div>\
+    <form method='post'>\
+\
+    <div style='background-color:lightgrey; padding:20px;'>\
+    <h2>Current WiFi Settings</h2>\
+    <p>IP  : %d.%d.%d.%d</p>\
+    <p style='font-size:80%'>SSID:<br><input type='text' name='ssid' value='%s' size='31'></p>\
+    <p style='font-size:80%'>Password:<br><input type='text' name='pass' value='%s' size='31'></p>\
+    <p style='font-size:80%'>DNS Name:<br><input type='text' name='dns' value='%s' size='31'></p>\
+    <p style='font-size:80%'>Start in AP mode\
+    <input type='radio' name='ap_mode' value='yes' %s>Yes\
+    <input type='radio' name='ap_mode' value='no' %s>No\
+    </p>\
+    </div>\
+\
+    <div padding:20px><br></div>\
+\
+    <div style='background-color:lightgrey; padding:20px;'>\
+    <h2>Select function of GPIO<b>0</b></h2>\
+    <p style='font-size:80%'>Mode\
+    <input type='radio' name='gpio0_mode' value='0' %s>Input\
+    <input type='radio' name='gpio0_mode' value='1' %s>Output\
+    </p>\
+    <p style='font-size:80%'>Value\
+    <input type='radio' name='gpio0_value' value='0' %s>0\
+    <input type='radio' name='gpio0_value' value='1' %s>1\
+    </p>\
+    <p style='font-size:80%'>URL on low/high transition<br><input type='text' name='gpio0_low2high' value='%s' size='63'></p>\
+    <p style='font-size:80%'>URL on high/low transition<br><input type='text' name='gpio0_high2low' value='%s' size='63'></p>\
+    </div>\
+\
+    <div padding:20px><br></div>\
+\
+    <div style='background-color:lightgrey; padding:20px;'>\
+    <h2>Select function of GPIO<b>2</b></h2>\
+    <p style='font-size:80%'>Mode\
+    <input type='radio' name='gpio2_mode' value='0' %s>Input\
+    <input type='radio' name='gpio2_mode' value='1' %s>Output\
+    </p>\
+    <p style='font-size:80%'>Value\
+    <input type='radio' name='gpio2_value' value='0' %s>0\
+    <input type='radio' name='gpio2_value' value='1' %s>1\
+    </p>\
+    <p style='font-size:80%'>URL on low/high transition<br><input type='text' name='gpio2_low2high' value='%s' size='63'></p>\
+    <p style='font-size:80%'>URL on high/low transition<br><input type='text' name='gpio2_high2low' value='%s' size='63'></p>\
+    </div>\
+\
+    <div padding:20px><br></div>\
+\
+    <div style='background-color:lightgrey; padding:20px;'>\
+    <p style='font-size:80%'><input type='submit' value='Save'><input type='reset' value='Reset'><br>\
+    <input type='checkbox' name='reset' value='yes'>Reset to defaults<br>\
+    <input type='checkbox' name='reboot' value='yes'>Reboot after saving<br></p>\
+    </div>\
+\
+  </form>\
+\
+</body>\
+</html>\
+";
+
